c5b8e2b427223b62781f4f852f64db9a5b3916f3
Kelia Simbi <simbikelia@gmail.com>
2025-11-09 17:55:11 +0200

    Initialize not-found page

---
+++ simbispace/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function notfound() {
+    return (
+        <div>
+            <h1 className="text-3xl text-[#A18072]">404</h1>
+            <h2>Oops! Page not found, try again</h2>
+        </div>
+    );
+}
--- simbispace/app/page.tsx
+++ simbispace/app/page.tsx
@@ -2,12 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-50">
-        Welcome to SimbiSpace's corner on the internet!!!!! <br/>  What would you like to visit?<br />
-        <a href="">View Portfolio</a><br />
-        <a href="">Visit Blog</a>
+    <div className="min-h-screen flex bg-[#F7EEE9]">
+    <div className="items-center justify-center bg-[#F7EEE9] font-sans">
+      <h1 className="text-4xl font-bold text-[#A18072]">
+        Welcome to SimbiSpace
       </h1>
-    </div>
+    </div></div>
   );
 }
