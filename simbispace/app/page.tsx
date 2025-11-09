0424285a227c0e004735910781e7e149671d7750
Kelia Simbi <simbikelia@gmail.com>
2025-11-09 18:07:07 +0200

    Add styling to homepage

--- simbispace/app/page.tsx
+++ simbispace/app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex bg-[#F7EEE9]">
-    <div className="items-center justify-center bg-[#F7EEE9] font-sans">
-      <h1 className="text-4xl font-bold text-[#A18072]">
-        Welcome to SimbiSpace
-      </h1>
-    </div></div>
+    
+    <div className="bg-[#F7EEE9] min-h-screen flex flex-col items-center justify-center">
+      <h2 className="text-6xl text-[#A18072] font-bold mb-5 ">Welcome to SimbiSpace</h2>
+      <span><i>A personal blog made by Kelia</i></span>
+      <a className="bg-[#8B6F47] text-white font-semibold px-4 py-4 rounded-4xl mt-8" href="">View my inner thoughts!</a>
+    </div>
   );
 }
