88380840414db8a4facdf7e06c9b10ec1dd1f740
Kelia Simbi <simbikelia@gmail.com>
2025-11-10 11:59:23 +0200

    Remove the nested folder

--- simbispace/app/blogs/[blogId]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function blogs({params}:{params: Promise< {blogId: string}>}) {
-    const blogId=  (await params).blogId
-    return (
-        <div>
-            <h1 className="font-bold text-5xl">Blog Number{blogId}</h1>
-        </div>
-    );
-}
--- simbispace/app/blogs/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Blogs() {
-    return (
-        <div>
-            <h1>View a list of blogs I created!</h1>
-        </div>
-    );
-}
