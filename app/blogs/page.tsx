852ad7bb0fd3f59375dec19d16936eff06a56064
Kelia Simbi <simbikelia@gmail.com>
2025-11-16 20:28:33 +0200

    Fix: Correct font class name for consistency in Blogs component

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -4,7 +4,7 @@ export default function Blogs() {
     const articles = getCategorisedArticles();
     return (
        <section className="mx-auto w-11/12 md:w-1/2 mt-20 flex flex-col gap-16 mb-20">
-        <header className="font-CormorantGaramond font-light text-6xl text-neutral-900 text-center">
+        <header className="font-cormorant-garamond font-light text-6xl text-neutral-900 text-center">
             <h1>View a list of blogs I created!</h1>
 
         </header>
