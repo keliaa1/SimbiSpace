94c86e59b8a69803dc503ffbcb261695c5751803
Kelia Simbi <simbikelia@gmail.com>
2025-11-16 16:34:40 +0200

    Refactor: Enhance article fetching and sorting logic

--- lib/articles.ts
+++ lib/articles.ts
@@ -4,3 +4,35 @@ import matter from "gray-matter";
 import path from "path";
 import { remark } from "remark";
 import html from "remark-html";
+
+import type { ArticleItem } from "@/types";
+import moment from "moment";
+
+const articlesDirectory = path.join(process.cwd(), "articles");
+const getSortedArticles = ():ArticleItem[]=>{
+    const fileNames = fs.readdirSync(articlesDirectory);
+
+    const allArticlesData = fileNames.map((fileName)=>{
+        const id = fileName.replace(/\.md$/, "");
+        const fullPath = path.join(articlesDirectory, fileName);
+        const fileContents = fs.readFileSync(fullPath, "utf8");
+        const matterResult = matter(fileContents);
+
+        return{
+            id,
+            title: matterResult.data.title,
+            date: matterResult.data.date,
+            category: matterResult.data.category,
+        }
+    })
+
+    return allArticlesData.sort((a,b)=>{
+        const format = "DD-MM-YYYY";
+        const dateOne = moment(a.date, format);
+        const dateTwo = moment(b.date, format);
+        if (dateOne.isBefore(dateTwo)) return -1;
+        if (dateTwo.isBefore(dateOne)) return 1;
+        return 0;
+    })
+
+}
