965c1108165c41eb394755f7e7a5fce323ec1139
Kelia Simbi <simbikelia@gmail.com>
2025-11-16 17:41:20 +0200

    Refactor: Replace blog component with Article component for improved article display

--- app/blogs/[blogId]/page.tsx
+++ app/blogs/[blogId]/page.tsx
@@ -1,8 +1,22 @@
-export default async function blogs({params}:{params: Promise< {blogId: string}>}) {
-    const blogId=  (await params).blogId
-    return (
-        <div>
-            <h1 className="font-bold text-5xl">Blog Number{blogId}</h1>
-        </div>
-    );
+import Link from "next/link";
+import { ArrowLeftCircleIcon } from "@heroicons/react/16/solid";
+import { getArticleData } from "@/lib/articles";
+
+const Article = async ({params}:{params:{slug:string}})=>{
+    const articleData = await getArticleData(params.slug);
+    return(
+        <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
+            <div className="flex justify-between font-poppins">
+<Link href={"/"} className="flex flex-row gap-1 place-items-center">
+                <ArrowLeftCircleIcon className="w-6 h-6" />
+                <span>Back</span>
+
+</Link>
+
+<p>{articleData.date}</p>
+            </div>
+            <article dangerouslySetInnerHTML={{__html: articleData.contentHtml}} />
+        </section>
+    )
 }
+export default Article
