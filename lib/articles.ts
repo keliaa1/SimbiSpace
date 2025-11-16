f8eec525060f422e5ba756c5584d53b4cd4e7a31
Kelia Simbi <simbikelia@gmail.com>
2025-11-16 17:28:24 +0200

    Refactor: Improve article data fetching and processing logic

--- lib/articles.ts
+++ lib/articles.ts
@@ -7,6 +7,7 @@ import html from "remark-html";
 
 import type { ArticleItem } from "@/types";
 import moment from "moment";
+import { title } from "process";
 
 const articlesDirectory = path.join(process.cwd(), "articles");
 const getSortedArticles = ():ArticleItem[]=>{
@@ -50,4 +51,23 @@ export const getCategorisedArticles= ():Record<string, ArticleItem[]>=>{
     })
 
     return categorisedArticles;
+}
+
+export const getArticleData = async (id:string)=>{
+    const fullPath = path.join(articlesDirectory, `${id}.md`)
+
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const matterResult = matter(fileContents);
+
+    const processedContent = await remark().use(html).process(matterResult.content);
+    const contentHtml = processedContent.toString();
+
+    return{
+        id,
+        contentHtml,
+        title: matterResult.data.title,
+        date: moment(matterResult.data.date).format("DD-MM-YYYY"),
+        category: matterResult.data.category
+    }
+
 }
