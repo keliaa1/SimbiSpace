645e153b2832a0ad63398f78945a06ab973da3c3
Kelia Simbi <simbikelia@gmail.com>
2025-11-16 16:09:52 +0200

    Refactor: Update Blogs component structure

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,7 +1,12 @@
 export default function Blogs() {
     return (
-        <div>
+       <section className="mx-auto w-11/12 md:w-1/2 mt-20 flex flex-col gap-16 mb-20">
+        <header className="font-CormorantGaramond font-light text-6xl text-neutral-900 text-center">
             <h1>View a list of blogs I created!</h1>
-        </div>
+
+        </header>
+        <section className="md:grid md:grid-cols-w flex flex-col gap-10"></section>
+
+       </section>
     );
 }
